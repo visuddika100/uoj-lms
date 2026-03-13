71a9bf0f2ca169321bc7e2663b9f98db14fbb3b4
visuddika100 <visukaushi@gmail.com>
2026-03-13 06:11:57 +0530

    change usermode and authroutes

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["student", "admin"], default: "student" }
-}, { timestamps: true });
+
+  name: String,
+
+  email: {
+    type: String,
+    unique: true
+  },
+
+  password: String,
+
+  regNo: String,
+
+  role: {
+    type: String,
+    default: "student"
+  }
+
+});
 
 module.exports = mongoose.model("User", userSchema);
  
